730a2e344a92c553db5e9850f5ae66fee7411cb5
alec dye <alecdye@gmail.com>
2021-06-14 15:59:27 -0700

    Add alt desc to spinner img

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import spinner from "../img/spinner.gif"
 
 const Spinner = () => {
   return (
-    <img src={spinner} style={{width: '200px', margin:"auto", display:"block"}} />
+    <img src={spinner} style={{width: '200px', margin:"auto", display:"block"}} alt="loading animation" />
   )
 }
 
